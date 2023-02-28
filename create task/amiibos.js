7aebff9e8fdd4db4c48c5c978bd16e0831de73ae
jessychui <jessy.chui@gmail.com>
2023-02-28 13:53:39 -0500

    ???

---
+++ create task/amiibos.js
@@ -0,0 +1,21 @@
+const amiiboSeries = {
+  supermario: [
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00000000-00340102.png",
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00010000-00350102.png",
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00020000-00010002.png",
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00030000-00020002.png",
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00050000-00140002.png",
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00070000-001a0002.png",
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00130000-02660102.png",
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00140000-02670102.png",
+  ],
+  ac: [
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_01810000-037d0002.png",
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_01820000-02400502.png",
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_01830000-02420502.png",
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_01850001-004b0502.png",
+    "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_01860101-00af0502.png",
+  ],
+};
+
+export { amiibos };
--- create task/main.js
+++ create task/main.js
@@ -8,8 +8,23 @@ const DOMSelectors = {
   metroid: document.getElementById("metroid"),
   splatoon: document.getElementById("splatoon"),
 };
-
+let amiibos = "did not work lol";
 async function getData() {
   const response = await fetch("https://www.amiiboapi.com/api/amiibo");
   const data = await response.json();
+  amiibos = data.amiibo;
+  console.log(data);
+  console.log(amiibos);
+  return amiibos;
 }
+
+DOMSelectors.ac.addEventListener("click", function () {
+  let marioAmiibo = amiibos.filter(
+    (marioAmiibo) => amiibos.amiiboSeries === "Animal Crossing"
+  );
+  console.log(marioAmiibo);
+});
+getData();
+
+/* if (data.amiibo.amiiboSeries === "Super Mario") {
+    console.log(data.amiibo); */
